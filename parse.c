e281c01fdd9a3e8300d958dbb9547741b0601e40
Anthony <alam6@stuy.edu>
2018-11-14 11:44:34 -0500

    started

---
+++ parse.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include <unistd.h>
+
+char ** parse_args(char * line){
+  char * args[5];
+  int i = 0;
+  while(line){
+    strsep(&line, " ");
+  
+
+
+int main(){
+  return 0;
+}
